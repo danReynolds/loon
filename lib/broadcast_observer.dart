3580c84cc990776bfe48acc78c19f551003742e0
Dan Reynolds <me@danreynolds.ca>
2023-08-24 08:35:49 -0400

    fix persistor bug

--- lib/broadcast_observer.dart
+++ lib/broadcast_observer.dart
@@ -3,8 +3,11 @@ part of loon;
 /// Extends an [Observable] by registering it to receive document broadcasts.
 mixin BroadcastObserver<T> on Observable<T> {
   @override
-  void init(T initialValue) {
-    super.init(initialValue);
+  void init(
+    T initialValue, {
+    required bool multicast,
+  }) {
+    super.init(initialValue, multicast: multicast);
     Loon._instance._addBroadcastObserver(this);
   }
 
