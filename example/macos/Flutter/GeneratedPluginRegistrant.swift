c5bcb2d6cfc574fd470af5cc4ef016161fc15508
Ray Li <ray.li.verified@gmail.com>
2026-04-15 17:43:48 -0500

    Update flutter_secure_storage v10.0.0 (#30)

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_secure_storage_macos
+import flutter_secure_storage_darwin
 import path_provider_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
--- lib/persistor/index.dart
+++ lib/persistor/index.dart
@@ -1,5 +1,5 @@
 export './stubs/stub_indexed_db_persistor.dart'
-    if (dart.library.html) './indexed_db_persistor/indexed_db_persistor.dart';
+    if (dart.library.js_interop) './indexed_db_persistor/indexed_db_persistor.dart';
 export './stubs/stub_file_persistor.dart'
     if (dart.library.io) './file_persistor/file_persistor.dart';
 export './stubs/stub_sqlite_persistor.dart'
